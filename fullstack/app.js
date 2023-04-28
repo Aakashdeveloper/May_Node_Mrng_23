9903ea545379705217f6c4224fe1cf448a82d495
aakashdeveloper <ahanda205@gmail.com>
2023-04-28 09:51:31 +0530

    ejs

--- fullstack/app.js
+++ fullstack/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8811;
-const categoryRouter = require('./src/controller/categoryRouter')
-const productRouter = require('./src/controller/productRouter')
+const categoryRouter = require('./src/controller/categoryRouter');
+const productRouter = require('./src/controller/productRouter');
+
+//middleware supporting lib
+// static file path
+app.use(express.static(__dirname+'/public'))
+// html files path
+app.set('views','./src/views')
+// view engine name
+app.set('view engine','ejs')
 
 //routes
 app.get('/',function(req,res){
-    res.send('Hii From Express Default route')
+    //res.send('Hii From Express Default route')
+    res.render('index',{title:'Home Page'})
 })
 
 app.use('/category',categoryRouter)
