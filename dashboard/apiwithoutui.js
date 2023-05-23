aa8495fe6e1648ad12937e2c1aa32fa19ec0a021
aakashdeveloper <ahanda205@gmail.com>
2023-05-23 09:51:02 +0530

    swagger

--- dashboard/apiwithoutui.js
+++ dashboard/apiwithoutui.js
@@ -13,6 +13,12 @@ const collection = client.db('internfeb').collection('dashboard');
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const port = process.env.PORT || 7710;
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const package = require('./package.json');
+
+swaggerDocument.info.version = package.version;
+app.use('/api-doc',swaggerUi.serve,swaggerUi.setup(swaggerDocument))
 
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -31,12 +37,25 @@ app.post('/addUser',async(req,res)=>{
 app.get('/users',async(req,res)=>{
     const output = [];
     let query = {};
-    if(req.query.city){
+    if(req.query.city && req.query.role){
+        query={
+            city:req.query.city,
+            role:req.query.role,
+            isActive:true
+        }
+    }
+    else if(req.query.city){
         query={
             city:req.query.city,
             isActive:true
         }
-    }else if(req.query.isActive){
+    }else if(req.query.role){
+        query={
+            role:req.query.role,
+            isActive:true
+        }
+    }
+    else if(req.query.isActive){
         let isActive = req.query.isActive;
         if(isActive == "false"){
             isActive = false
@@ -54,6 +73,72 @@ app.get('/users',async(req,res)=>{
     cursor.closed;
     res.send(output)
 })
+
+app.get('/user/:id',async(req,res) => {
+    const output = [];
+    let query = {_id:new Mongo.ObjectId(req.params.id)}
+    const cursor = collection.find(query);
+    for await(const data of cursor){
+        output.push(data)
+    }
+    cursor.closed;
+    res.send(output)
+})
+
+
+// update user
+app.put('/updateUser',async(req,res) => {
+    await collection.updateOne(
+        {_id:new Mongo.ObjectId(req.body._id)},
+        {
+            $set:{
+                name:req.body.name,
+                city:req.body.city,
+                phone:req.body.phone,
+                role:req.body.role,
+                isActive:true
+            }
+        }
+    )
+    res.send('Record Updated')
+})
+
+// hard delete user
+app.delete('/deleteUser',async(req,res) => {
+    await collection.deleteOne({
+        _id:new Mongo.ObjectId(req.body._id)
+    })
+
+    res.send('User Deleted')
+})
+
+// soft delete (deactivate)
+app.put('/deactivateUser',async(req,res) => {
+    await collection.updateOne(
+        {_id:new Mongo.ObjectId(req.body._id)},
+        {
+            $set:{
+                isActive:false
+            }
+        }
+    )
+    res.send('User Deactivated')
+})
+
+//  (Activate user)
+app.put('/activateUser',async(req,res) => {
+    await collection.updateOne(
+        {_id:new Mongo.ObjectId(req.body._id)},
+        {
+            $set:{
+                isActive:true
+            }
+        }
+    )
+    res.send('User activated')
+})
+
+
 
 app.listen(port,() => {
     main();
