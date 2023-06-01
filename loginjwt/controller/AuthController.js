50712ec4dd4fed31e2e502491378259a31251c04
aakashdeveloper <ahanda205@gmail.com>
2023-06-01 09:16:21 +0530

    loginjwt

--- loginjwt/controller/AuthController.js
+++ loginjwt/controller/AuthController.js
@@ -9,6 +9,47 @@ const User = require('../model/userScehma')
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
+//register User
+router.post('/register',(req,res) => {
+    let hashpassword = bcrypt.hashSync(req.body.password,8)
+    User.create({
+        name:req.body.name,
+        email:req.body.email,
+        phone:req.body.phone,
+        password:hashpassword,
+        role:req.body.role?req.body.role:'User'
+    },(err,result) => {
+        if(err) return res.status(500).send(`There is problem in Registration`)
+        res.status(200).send('Registration successful')
+    })
+})
+
+router.post('/login',(req,res) => {
+    User.findOne({email:req.body.email},(err,user) =>{
+        if(err) return res.status(500).send({auth:false,token:'There is problem while login'});
+        if(!user) return res.status(500).send({auth:false,token:'No User Found register first'});
+        else{
+            const passIsValid = bcrypt.compareSync(req.body.password,user.password);
+            if(!passIsValid) return res.status(401).send({auth:false,token:'Invalid Password'});
+            var token = jwt.sign({id:user._id},config.secert,{expiresIn:86400});
+            return res.status(200).send({auth:true,token:token});
+        }
+    })
+})
+
+//get User info
+router.get('/userInfo',(req,res) => {
+    let token = req.headers['x-access-token'];
+    if(!token) res.send({auth:false,token:'No Token Provided'})
+    jwt.verify(token,config.secert,(err,data) => {
+        if(err) return res.status(500).send({auth:false,token:'Invalid Token Provided'});
+        User.findById(data.id,(err,user) => {
+            if(err) return res.status(500).send({auth:false,token:'Error Fetching user'});
+            res.json(user)
+        })
+    })
+})
+
 
 // list all users
 router.get('/users',(req,res) => {
@@ -19,4 +60,4 @@ router.get('/users',(req,res) => {
 })
 
 
-module.export = router
+module.exports = router
--- loginjwt/db.js
+++ loginjwt/db.js
@@ -1,2 +1,2 @@
 let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/internfeb');
+mongoose.connect('mongodb://127.0.0.1:27017/internfeb');
